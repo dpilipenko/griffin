eb2e3ed2a4d1088219837c32cf21ceb894f2a50d
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2015-08-16 11:00:38 -0400

    Add logging filter

---
+++ grails-app/conf/griffin/filters/LoggingFilterFilters.groovy
@@ -0,0 +1,29 @@
+package griffin.filters
+
+import grails.plugin.springsecurity.SpringSecurityService
+import griffin.domain.User
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory;
+
+class LoggingFilterFilters {
+
+	SpringSecurityService springSecurityService
+	
+    def filters = {
+        all(controller:'*', action:'*') {
+            before = {
+				log.trace "controller action w/ this request: $request.requestURL ? $request.queryString"
+				User user = springSecurityService.currentUser
+				log.trace "current user is: " + user
+            }
+            after = { Map model ->
+
+            }
+            afterView = { Exception e ->
+
+            }
+        }
+    }
+	
+	private static final Log log = LogFactory.getLog(this)
+}
---
+++ test/unit/griffin/filters/LoggingFilterFiltersSpec.groovy
@@ -0,0 +1,17 @@
+package griffin.filters
+
+import grails.test.mixin.Mock
+import spock.lang.Specification
+
+@Mock(LoggingFilterFilters)
+class LoggingFilterFiltersSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
